4d5edbbedd8afbf61a2da5ff7add074d56fcfb95
Sanika <sanika8356@gmail.com>
2025-06-26 23:56:38 +0530

    final

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from typing import Any
-from dotenv import load_dotenv
 
-load_dotenv()
-
-DATABASE_URL: str = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL not set in .env file")
+DATABASE_URL = "url here"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base: Any = declarative_base()
+Base = declarative_base()
